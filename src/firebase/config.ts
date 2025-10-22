c68067e1180e3284f36b0b85cec4788ffc36f2fe
Dieumerci Maombi luc <dieumercimaombi2@gmail.com>
2025-10-22 12:27:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4474823376-7f7c4",
+  "appId": "1:308494677405:web:f79620beab1b9d02db6b74",
+  "apiKey": "AIzaSyDdayzdzsU2M6Q-ri2FSYwlnUBibMTNvB0",
+  "authDomain": "studio-4474823376-7f7c4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "308494677405"
+};
